aa23b0cc7d01146f955509241868b3e0ea3a7f24
Fellin_sleep <110546006+Sunja-An@users.noreply.github.com>
2022-09-24 11:01:39 +0000

    add Register function

--- WEB(FE)/ar/src/features/counterSlice.js
+++ WEB(FE)/ar/src/features/counterSlice.js
@@ -2,52 +2,18 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { fetchCount } from './counterAPI';
 
 const initialState = {
-  value: 0,
-  status: 'idle',
+  User_ID : '',
+  LoggedIn : false,
+  User_Last_Date : Date.now(),
+  
 };
 
-export const incrementAsync = createAsyncThunk(
-  'counter/fetchCount',
-  async (amount) => {
-    const response = await fetchCount(amount);
-    // The value we return becomes the `fulfilled` action payload
-    return response.data;
-  }
-);
-
-export const counterSlice = createSlice({
-  name: 'counter',
+const UserState = createSlice({
+  name : 'User',
   initialState,
-  reducers: {
-    increment: (state) => {
-      state.value += 1;
-    },
-    decrement: (state) => {
-      state.value -= 1;
-    },
-    incrementByAmount: (state, action) => {
-      state.value += action.payload;
-    },
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(incrementAsync.pending, (state) => {
-        state.status = 'loading';
-      })
-      .addCase(incrementAsync.fulfilled, (state, action) => {
-        state.status = 'idle';
-        state.value += action.payload;
-      });
-  },
-});
+  reducer : {
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
-export const selectCount = (state) => state.counter.value;
-export const incrementIfOdd = (amount) => (dispatch, getState) => {
-  const currentValue = selectCount(getState());
-  if (currentValue % 2 === 1) {
-    dispatch(incrementByAmount(amount));
   }
-};
+})
 
-export default counterSlice.reducer;
+export default UserState.reducer;
--- WEB(FE)/ar/src/routes/Register.js
+++ WEB(FE)/ar/src/routes/Register.js
@@ -3,13 +3,15 @@ useState,
 useEffect,
 useRef,
 } from 'react'
+import Dropdown from 'react-bootstrap/Dropdown';
+
 
 const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{8,16}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 
 const Register = () => {
-    const UserRef = useRef(null);
-    const errRef = useRef(null);
+    const UserRef = useRef();
+    const errRef = useRef();
     
     const [UserObj, setUserObj] = useState({
         'UserId' : '',
@@ -25,13 +27,52 @@ const Register = () => {
         'UserLastDate' : Date.now()
     });
 
+    const [errMsg, setErrMsg] = useState('');
+
     const [validId, setvalidId] = useState(false);
     const [validPwd, setvalidPwd] = useState(false);
     const [validUserName, setvalidUserName] = useState(false);
     const [validUserLocation, setvalidUserLocation] = useState(false);
     const [validUserLastDate, setvalidUserLastDate] = useState(false);
 
+    const [userFocus, setUserFocus] = useState(false);
+    const [pwdFocus, setPwdFocus] = useState(false);
+
+    const [matchPwd, setMatchPwd] = useState('');
+    const [validMatch, setValidMatch] = useState(false);
+    const [matchFocus, setMatchFocus] = useState(false);
+
     const { UserId, UserPwd, UserName, UserClasses, UserLocation, UserLastDate} = UserObj;
+
+    const onDropDownBtnClick = () =>{
+        const change = document.getElementById("dropdown_classes");
+        const Army_class = document.getElementsByClassName("dropdown_class");
+        change.innerText = Army_class.innerText;
+    }
+
+    useEffect(()=> {
+        UserRef.current?.focus();
+    }, [])
+
+    useEffect(() => {
+        const result = USER_REGEX.test(UserObj.UserId);
+        console.log(result);
+        console.log(UserObj.UserId);
+        setvalidId(result);
+    }, [UserObj.UserId]);
+
+    useEffect(() => {
+        const result = PWD_REGEX.test(UserObj.UserPwd);
+        console.log(result);
+        console.log(UserObj.UserPwd);
+        setvalidPwd(result);
+        const match = UserObj.UserPwd === matchPwd;
+        setValidMatch(match);
+    }, [UserObj.UserPwd, matchPwd]);
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [UserObj.UserId, UserObj.UserPwd, matchPwd]);
 
     const onChange = (event) => {
         const {
@@ -48,49 +89,74 @@ const Register = () => {
         
     }
 
-    const ClassDropDown = () => {
-        document.getElementsByClassName("Dropdown_Btn").classList.toggle('show');
-    };
-    function DropDown(e){
-        if(e.target.matches('Dropdown-Content')){
-            let dropdowns = document.getElementByClassName('Dropdown-Content');
-            let i;
-            for (i=0;i<dropdowns.length();i++){
-                let openDropdown = dropdowns[i];
-                if (openDropdown.classList.contains('show')){
-                    openDropdown.classList.remove('show');
-                }
-            }
-        }
-    };
 
     return (
         <div className="AR_User_Making">
+            <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"}  aria-live="assertive">
+                {errMsg}
+            </p>
             <form onSubmit={onSubmit}>
                 <h1>Register</h1>
                 <div className="AR_Topic_Info">
                     <div className="Designate_Id">
-                        <label For="UserId" className='UserId'>ID</label>
+                        <label htmlFor="UserId" className='UserId'>
+                            ID
+                            <span className={validUserName ? "valid" : "hide"}>
+                                됐음!
+                            </span>
+                            <span className={validId || !UserObj.id ? "hide" : "invalid"}>
+                                안됐음.
+                            </span>
+                        </label>
                         <input
                             name="UserId"
                             type="text"
                             onChange={onChange}
+                            autoComplete="off"
                             placeholder='ID를 입력해주세요.'
                             required
+                            aria-invalid={validId ? "false" : "true"}
+                            aria-describedby="uidnote"
+                            onFocus={() => setUserFocus(true)}
+                            onBlur={() => setUserFocus(false)}
                         />
                     </div>
+                    <p id="uidnote" className={userFocus && UserId && 
+                    !validId ? "instructions" : "offscreen"}>
+                        8글자에서 16글자 사이여야 합니다. <br/>
+                        반드시 영문으로 시작되어야 합니다. <br/>
+                        문자, 숫자, _ , !, @, #, $, % 등이 가능 합니다.
+                    </p>
                     <div className="Designate_Pwd">
-                        <label For="UserPwd" className='UserPwd'>Password</label>
+                        <label htmlFor="UserPwd" className='UserPwd'>
+                            Password
+                            <span className={validPwd ? "valid" : "hide"}>
+                                됐음.
+                            </span>
+                            <span className={validPwd || !UserObj.UserPwd ? "hide" : "invalid"}>
+                                안됐음.
+                            </span>
+                        </label>
                         <input
-                            name="UserPwd"
                             type="password"
+                            id="password"
                             onChange={onChange}
                             placeholder='비밀번호를 입력해주세요'
                             required
+                            aria-invalid={validPwd ? "false" : "true"}
+                            aria-describedby="pwdnote"
+                            onFocus={()=> setPwdFocus(true)}
+                            onBlur={()=>setPwdFocus(false)}
                         />
+                        <p id="pwdnote" className={pwdFocus && !validPwd ? "instruction" :
+                        "offscreen"}>
+                            8글자에서 24글자 사이여야 합니다. <br/>
+                            대문자, 소문자, 숫자, 특수문자를 포함해야 합니다. <br/>
+                            다음과 같은 특수문자를 허용합니다. ! @ # $ % <br/>
+                        </p>
                     </div>
                     <div className="Designate_Name">
-                        <label For="UserName" className="UserName">이름</label>
+                        <label htmlFor="UserName" className="UserName">이름</label>
                         <input 
                             name="UserName"
                             type="text" 
@@ -100,13 +166,17 @@ const Register = () => {
                         />    
                     </div>
                     <div className="AR_Classes_Dropdown">
-                        <button onClick={ClassDropDown} className='Dropdown_Btn'>계급</button>
-                        <div className="Dropdown-Content">
-                            <span>이병</span>
-                            <span>일병</span>
-                            <span>상병</span>
-                            <span>병장</span>
-                        </div>
+                        <Dropdown>
+                            <Dropdown.Toggle variant="success" id="dropdown_classes">
+                                선택
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item onClick={onDropDownBtnClick} className="dropdown_class">이병</Dropdown.Item>
+                                <Dropdown.Item onClick={onDropDownBtnClick} className="dropdown_class">일병</Dropdown.Item>
+                                <Dropdown.Item onClick={onDropDownBtnClick} className="dropdown_class">상병</Dropdown.Item>
+                                <Dropdown.Item onClick={onDropDownBtnClick} className="dropdown_class">병장</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
                     </div>
                     <div className="AR_User_Location">
 
