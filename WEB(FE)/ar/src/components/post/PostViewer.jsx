17c19302638bdf270d763d0dc618a777f4b91e41
GyeongChan <40288646+gclogs@users.noreply.github.com>
2022-10-06 06:19:34 +0000

    add: getter and setter implementation

--- WEB(FE)/ar/src/components/post/PostViewer.jsx
+++ WEB(FE)/ar/src/components/post/PostViewer.jsx
@@ -1,15 +1,46 @@
-import React, { Component } from 'react';
-import PostLetter from './LetterHead';
-import PostLetterInput from './LetterInput';
-import PostSuggestion from './Suggest';
+import PostLetter from './PostLetter';
+import PostSuggest from './PostSuggest';
+import PostList from './PostList';
+import db from '../../database/DB_Manager';
+import { setDoc, addDoc, collection, query, getDocs } from 'firebase/firestore';
+
+export class Post {
+  contructor(userName, content) {
+    this.userId = Math.random().toString().slice(2);
+    this.userName = userName
+    this.content = content; 
+    this.date = new Intl.DateTimeFormat('kr', {dateStyle: 'full', timeStyle: 'short'}).format(new Date());
+  }
+
+  /**
+   * @param payload Location pointing to the collection => 'letters || suggests'
+   * @param data data with object type
+   */
+  set(payload, data="object") {
+    setDoc(collection(db, "posts", payload), data);
+  }
+
+  /**
+   * @param payload Location pointing to the collection => 'letters || suggests'
+   */
+  get(payload) {
+    const q = query("posts", payload);
+    const data = getDocs(q);
+    const newData = data.docs.map(doc => ({
+      ...doc.data()
+    }))
+
+    return newData;
+  }
+}
 
 const PostViewer = () => {
   
   return (
     <>
       <PostLetter></PostLetter>
-      <PostLetterInput></PostLetterInput>
-      <PostSuggestion></PostSuggestion>
+      <PostSuggest></PostSuggest>
+      <PostList></PostList>
     </>
   )
 }
