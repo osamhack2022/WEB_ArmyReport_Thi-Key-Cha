51e9b94aa83d9bd2518847a898b8bd4a68055961
GyeongChan <woig9874@naver.com>
2022-10-08 14:55:06 +0000

    add: profile setting the components to work with

---
+++ WEB(FE)/ar/src/components/base/HeaderUserIcon.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components'
+
+const HeaderUserIconBlock = styled.div`
+  cursor: pointer;
+
+`
+const HeaederUserIcon = () => {
+  
+}
+
+export default HeaederUserIcon;
---
+++ WEB(FE)/ar/src/components/base/HeaderUserMenu.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components'
+
+const HeaederUserMenuBlock = styled.div`
+  cursor: pointer;
+
+`
+const HeaederUserMenu = () => {
+  
+}
+
+export default HeaederUserMenu;
