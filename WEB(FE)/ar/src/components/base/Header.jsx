eeebdfcccacd75b3b4b8a630f611ef68b4548423
GyeongChan <woig9874@naver.com>
2022-10-08 14:53:15 +0000

    fix: minor changes

--- WEB(FE)/ar/src/components/base/Header.jsx
+++ WEB(FE)/ar/src/components/base/Header.jsx
@@ -1,41 +1,34 @@
-import { Link } from 'react-router-dom';
 import useHeader from './hooks/useHeader';
 import styled from 'styled-components'
-
-import { PersistGate } from 'redux-persist/integration/react';
-import store, {persistor} from '../../app/store'
-import {Provider} from 'react-redux';
+import HeaderMainMenu from './HeaderMainMenu'
 
 function Header(props) {
   const { user } = useHeader();
-  console.log(store);
-  
+
   return (
     <>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <Block>
-            <PostButton to="/post">마음의 편지</PostButton>
-          </Block>
-        </PersistGate>
-      </Provider>
+      <Block>
+        <HeaderInner className='in'>
+          <HeaderMainMenu user={user} />
+        </HeaderInner>
+      </Block>
     </>
   )
 }
 
-const Block = styled.div`
-  display: flex;
-  width: 100%;
-  height: 100px;
-  background: #222831;
-  a {
-    color: white;
-    text-decoration: none;
-  }
+const HeaderInner = styled.div`
+  width: 1920px;
+  margin: 0 auto;
+  position: relative;
 `
 
-const PostButton = styled(Link)`
-  display: flex;
+const Block = styled.div`
+  position: fixed;
+  top: 0;
+  background: #222831;
+  .in {
+    height: 80px
+  }
 `
 
 export default Header;
