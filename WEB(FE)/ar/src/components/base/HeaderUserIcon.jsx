d02cfbf4dfb6e074a0017c71cbf4b34231731e1b
GyeongChan <woig9874@naver.com>
2022-10-09 14:56:47 +0000

    feat: user menu visible

--- WEB(FE)/ar/src/components/base/HeaderUserIcon.jsx
+++ WEB(FE)/ar/src/components/base/HeaderUserIcon.jsx
@@ -1,12 +1,29 @@
-import { Link } from 'react-router-dom';
+import React from 'react';
+import IconUser from '../../static/svg/icon-user.svg'
 import styled from 'styled-components'
+import { HeaderIconBlock as Block } from './Header'
 
-const HeaderUserIconBlock = styled.div`
-  cursor: pointer;
-
+const User = styled.img`
+  position: absolute;
+  width: 22px;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  z-index: 1;
+  &:hover {
+    box-shadow: 0px 0 18px rgba(0, 0, 0, 0.3);
+  }
 `
-const HeaederUserIcon = () => {
-  
+
+const HeaederUserIcon = ({ onClick }) => {
+  return (
+    <>
+      <Block onClick={onClick}>
+        <User src={IconUser}/>
+      </Block>
+    </>
+  )
 }
 
 export default HeaederUserIcon;
