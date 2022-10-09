d2d17da3c95743b6f7ff797dd3963ec859b72e7d
GyeongChan <woig9874@naver.com>
2022-10-09 14:55:38 +0000

    fix: sort li and organize components / UI style

--- WEB(FE)/ar/src/components/base/HeaderMainMenu.jsx
+++ WEB(FE)/ar/src/components/base/HeaderMainMenu.jsx
@@ -1,41 +1,55 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components'
-import HeaderMainMenuContents from './HeaderMainMenuContents'
 
-const Wrap = styled.div`
+const HedaerMainMenuBlock = styled.div`
   position: absolute;
+  line-height: 3rem;
   bottom: 0;
-  left: 0;
-  z-index: 1;
+  left: 36px;
   display: flex;
-  
+  font-famliy: GothicA1 Regular;
+
   .route-item {
     padding: 0 12px;
     display: inline-block;
     color: white;
     font-size: 18px;
   }
+
+  a {
+    color: #ccc;
+    aliagn-items: cetner;
+    text-decoration: none;
+    &:hover {
+      color: white;
+      text-decoration: underline;
+    }
+  }
 `
 
 const HeaderMainMenu = ({user}) => {
   return (
     <>
-      <Wrap>
+      <HedaerMainMenuBlock>
         <ul>
           <li className='route-item'>
-            <Link to={`/${user.uid}`}>홈으로</Link>
+            <Link to="/post">
+              마음의 편지
+            </Link>
           </li>
           <li className='route-item'>
             <Link to="/post">
-              포스트
+              건의 사항
             </Link>
-            <HeaderMainMenuContents />
           </li>
           <li className='route-item'>
-            <Link>휴가</Link>
+            <Link to="/vaction">
+              휴가
+            </Link>
           </li>
         </ul>
-      </Wrap>
+      </HedaerMainMenuBlock>
     </>
   )
 }
