7599037a583f9120d2f60f57c222748dee1a18aa
GyeongChan <woig9874@naver.com>
2022-10-11 14:34:54 +0000

    add: fake loading component

---
+++ WEB(FE)/ar/src/components/Auth/AuthLoadFake.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import AuthLoadBackground from './AuthLoadBackground'
+
+const AuthLoadFake = () => {
+  const [loading, setLoaidng] = useState(true);
+
+  const FakeLoading = async () => {
+    setLoaidng(true);
+    await new Promise((delay) => {
+      setTimeout(delay, 3000)
+    });
+    setLoaidng(false);
+
+    return false;
+  }
+
+  useEffect(() => {
+    FakeLoading()
+  }, [])
+
+  return (
+    <>
+      {loading ? <AuthLoadBackground /> : null}
+    </>
+  )
+}
+
+export default AuthLoadFake;
