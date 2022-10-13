d33395ca5edf334c97a0c95e79b0918c0fd03c41
GyeongChan <40288646+gclogs@users.noreply.github.com>
2022-10-13 14:57:06 +0000

    add: user's information

---
+++ WEB(FE)/ar/src/components/profile/ProfileUserContainer.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+import Baby_Ogu_work from '../../static/baby-ogu-shovel.png'
+
+const ProfileUserContainerBlock = styled.div`
+  position: relative;
+  width: 1200px;
+`
+
+const Avatar = styled.span`
+  float: left;
+  margin-right: 18px;
+
+  img {
+    vertical-align: middle;
+  }
+`
+
+const ProfileUserContainer = ({ data }) => {
+  return (
+    <>
+      <ProfileUserContainerBlock>
+        <Avatar>
+          <img src={Baby_Ogu_work} />
+        </Avatar>
+        <span>{data.Userclass} {data.Username}</span>
+        <span>접속횟수: data.count_join</span>
+        <span>전역: data.discharge_week</span>
+        <span>휴가: data.vacation_week</span>
+      </ProfileUserContainerBlock>
+    </>
+  )
+}
+
+export default ProfileUserContainer;
