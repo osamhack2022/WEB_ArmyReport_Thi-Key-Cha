6cb991cad20c03a38332e2e1ba9c26f752a31376
Fellin_sleep <110546006+Sunja-An@users.noreply.github.com>
2022-10-13 14:55:38 +0000

    Put the authentication to show the component each users.

--- WEB(FE)/ar/src/components/Vacation/PersonPage.js
+++ WEB(FE)/ar/src/components/Vacation/PersonPage.js
@@ -1,20 +1,40 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Applicate from './Applicate';
-import { getVacation } from './hooks/V_Manager';
+import { getUserVacation } from './hooks/V_Manager';
+import styled from '@emotion/styled';
+import useHeader from '../base/hooks/useHeader';
+import { Typography } from '@mui/material';
+import { Button } from 'antd';
 
 const Item = styled(Paper)(({ theme }) => ({
-    ...theme.typography.body2,
     textAlign: 'center',
-    color: theme.palette.text.secondary,
     height: 60,
     lineHeight: '60px',
 }));
 
 
 const PersonPage = () => {
+    const user = useHeader();
+    const uid = user.uid;
+        
+    const [Userdata, setUserdata] = useState({
+        'Startdate' : new Date(),
+        'Enddate' : new Date(),
+        'Content' : '',
+        'Examine' : false
+    });
+    useEffect(()=>{
+        getUserVacation(uid).then((v)=>{
+            if (v === 'false'){
+                console.log('데이터가 없습니다..ㅠㅠㅠㅠ');
+            }else{
+                setUserdata(v);
+            }
+        });
+    }, [uid]);
     const [Open, setOpen] = useState(false);
     const onOpenhandle = () => {
         setOpen(Open => !Open);
@@ -32,25 +52,43 @@ const PersonPage = () => {
                     height: 500,
                     },
                 }}
-            >            
-                <Item elevation={3}>
-                    <Typography variant="h2" gutterBottom>
-                        내 휴가
-                    </Typography>
-                    <Typography variant="h4" gutterBottom>
-                        출발일 : 
-                    </Typography>
-                    <Typography variant="h4" gutterBottom>
-                        도착일 :
-                    </Typography>
-                    <Typography variant="h4" gutterBottom>
-                        휴가내용 : 
-                    </Typography>
-                </Item>
-                <Button onClick={onOpenhandle}>
-                    휴가신청!
-                    {Open && <Applicate />}
-                </Button>
+            >
+                {Userdata.Examine && 
+                <>
+                    <Item elevation={3}>
+                        <Typography variant="h2" gutterBottom>
+                            내 휴가
+                        </Typography>
+                        <Typography variant="h4" gutterBottom>
+                            출발일 : {Userdata.Startdate}
+                        </Typography>
+                        <Typography variant="h4" gutterBottom>
+                            도착일 : {Userdata.Enddate}
+                        </Typography>
+                        <Typography variant="h4" gutterBottom>
+                            휴가내용 : {Userdata.Content}
+                        </Typography>
+                    </Item>
+                    <Button onClick={onOpenhandle}>
+                        휴가신청!
+                        {Open && <Applicate />}
+                    </Button>
+                </>
+                }
+                {!Userdata.Examine && 
+                <>
+                    <Item elevation={3}>
+                        <Typography variant="h2" gutterBottom>
+                            신청한 휴가가 없습니다..
+                        </Typography>
+                    </Item>
+                    <Button onClick={onOpenhandle}>
+                        휴가신청!
+                        {Open && <Applicate />}
+                    </Button>
+                </>
+                }
+                
             </Box>
         </>
     );
