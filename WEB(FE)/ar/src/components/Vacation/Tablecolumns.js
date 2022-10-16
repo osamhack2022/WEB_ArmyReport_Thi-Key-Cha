083d5f6cc46d9b687e722a720df7711013f09336
AnSunWoo <110546006+Sunja-An@users.noreply.github.com>
2022-10-16 14:56:16 +0000

    Modify the data table ( etc. resizing, defining data )

--- WEB(FE)/ar/src/components/Vacation/Tablecolumns.js
+++ WEB(FE)/ar/src/components/Vacation/Tablecolumns.js
@@ -3,23 +3,23 @@ const moment = require('moment');
 export const basiccolumns = [
     { field: 'id', headerName: 'No', width: 60 },
     { field: 'Class', headerName: '계급', width: 70 },
-    { field: 'Name', headerName: '이름', width: 130 },
+    { field: 'Name', headerName: '이름', width: 100 },
     { field: 'Destination', headerName: '목적지', width: 130 },
     {
       field: 'Startdate',
       type: 'date',
       headerName: '출발일',
-      width: 90,
+      width: 180,
       valueFormatter: params => 
-        moment(params?.value).format("DD/MM/YYYY"),
+        moment(params?.value).format("YYYY-MM-DD"),
     },
     {
       field: 'Enddate',
       type: 'date',
       headerName: '도착일',
-      width: 90,
+      width: 180,
       valueFormatter: params => 
-        moment(params?.value).format("DD/MM/YYYY"),
+        moment(params?.value).format("YYYY-MM-DD"),
     },
     {
       field: 'Content',
@@ -36,6 +36,6 @@ export const basiccolumns = [
 ];
 
 export const OutcastColumn = [
-  { field: 'Away', headerName: '금일 휴가 출발자', width: 60 },
-  { field: 'Home', headerName: '금일 휴가 복귀자', width: 60 },
+  { field: 'Away', headerName: '금일 휴가 출발자', width: 135 },
+  { field: 'Home', headerName: '금일 휴가 복귀자', width: 135 },
 ]
