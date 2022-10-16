389a9e4a74d746371a190f1103463a3a532b570e
AnSunWoo <110546006+Sunja-An@users.noreply.github.com>
2022-10-16 14:53:51 +0000

    Add Desktopdatepicker to know the user vacation startdate and enddate for saving the data.

--- WEB(FE)/ar/src/components/Vacation/Applicate.js
+++ WEB(FE)/ar/src/components/Vacation/Applicate.js
@@ -2,19 +2,21 @@ import React, { useEffect, useRef, useState } from 'react';
 
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
 import TextField from '@mui/material/TextField';
 import LoadingButton from '@mui/lab/LoadingButton';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 import useHeader from '../base/hooks/useHeader';
 import { doc, getDoc, setDoc} from "firebase/firestore";
 
 import db from '../../database/DB_Manager';
 import { useSelector } from 'react-redux';
+import { Stack } from '@mui/material';
+import moment from 'moment';
 
 const style = {
     position: 'absolute',
@@ -26,11 +28,17 @@ const style = {
     border: '2px solid #000',
     boxShadow: 24,
     p: 4,
-  };
+};
 
-const Applicate = () => {
+
+
+const Applicate = ({ onComplete }) => {
     const {user} = useHeader();
     const userid = user.uid;
+
+    const [Startvalue, setStartvalue] = useState(moment(new Date(),"YYYY-MM-DD"));
+    const [Endvalue, setEndvalue] = useState(moment(new Date(),"YYYY-MM-DD"));
+
     const [UserData, setUserData] = useState({
         Name : '',
         Class : '',
@@ -53,7 +61,6 @@ const Applicate = () => {
     const NoteRef = useRef();
 
     useEffect(()=>{
-        getData(userid);
         setData(userid);
     }, []);
 
@@ -74,8 +81,8 @@ const Applicate = () => {
 
     async function setData(userid){
         await setDoc(doc(db,"02155004", "본부중대", "Vacation",`${userid}`),{
-            Username : UserData.Name,
-            Userclasses : UserData.Class,
+            Name : UserData.Name,
+            Class : UserData.Class,
             UserPhone : UserData.Number,
             Destination : '',
             Startdate : new Date(),
@@ -87,6 +94,7 @@ const Applicate = () => {
     };
 
     const onChange = (e) => {
+        console.log(e);
         const {
             target : {name, value}
         } = e;
@@ -97,6 +105,17 @@ const Applicate = () => {
         });
     }
 
+    const StarthandleChange = (Value) => {
+        
+        console.log(Value);
+        setStartvalue(Value);
+    };
+
+    const EndhandleChange = (Value) => {
+        setEndvalue(Value);
+        console.log(Value);
+    };
+
     const handleClose = () => {
         setOpen(false);
     };
@@ -104,24 +123,27 @@ const Applicate = () => {
     /* if compnay have Applicate, give the my state information for report my sickness. */
 
     const onhandleApplicate = async(e) => {
+        getData(userid);
         setLoading(true);
         e.preventDefault();
-
+        console.log(Startvalue);
+        console.log(Endvalue);
         await setDoc(doc(db, "02155004", "본부중대", "Vacation",`${userid}`), {
-            Username : UserData.Name,
-            Userclasses : UserData.Class,
+            Name : UserData.Name,
+            Class : UserData.Class,
             UserPhone : UserData.Number,
             Destination : UserData.Destination,
-            Startdate : UserData.Startdate,
-            Enddate : UserData.Enddate,
+            Startdate : Startvalue,
+            Enddate : Endvalue,
             Content : UserData.Content,
-            Note : UserData.Note
+            Note : UserData.Note,
+            Examine: false,
         });
 
         setTimeout(() => {
             setLoading(false);
             alert("보고하였습니다!");
-            handleClose();
+            onComplete();
         }, 1000);
     };
 
@@ -140,12 +162,28 @@ const Applicate = () => {
                         <FormLabel id="demo-controlled-radio-buttons-group">행선지</FormLabel>
                         <TextField name="Destination" inputRef={DesRef} label="ex) 서울 강북, 부산 해운대구" variant="outlined" onChange={onChange} required/>
                         
-                        <FormLabel id="demo-controlled-radio-buttons-group">출발일</FormLabel>
-                        <TextField name="Startdate" inputRef={StartdateRef} label="ex) 2022년 10월 1일" variant="outlined" onChange={onChange} required/>
+                        <Stack spacing={3}>
+                            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                <DesktopDatePicker
+                                    renderInput={(params) => <TextField {...params} name="startdate"/>}
+                                    label="출발일"
+                                    inputFormat="YYYY/MM/DD"
+                                    value={Startvalue}
+                                    onChange={StarthandleChange}
+                                />
+                            </LocalizationProvider>
+                
+                            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                <DesktopDatePicker
+                                    renderInput={(params) => <TextField {...params} name="startdate"/>}
+                                    label="도착일"
+                                    inputFormat="YYYY/MM/DD"
+                                    value={Endvalue}
+                                    onChange={EndhandleChange}
+                                />
+                            </LocalizationProvider>
+                        </Stack>
                         
-                        <FormLabel id="demo-controlled-radio-buttons-group">도착일</FormLabel>
-                        <TextField name="Enddate" inputRef={EnddateRef} label="ex) 2022년 10월 5일" variant="outlined" onChange={onChange} required/>
-
                         <FormLabel id="demo-controlled-radio-buttons-group">휴가 내용 기입</FormLabel>
                         <TextField name="Content" inputRef={ContentRef} label="ex) 연가 3일 모범용사 2일" variant="outlined" onChange={onChange} required/>
 
