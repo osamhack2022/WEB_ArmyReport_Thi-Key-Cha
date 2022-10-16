474e61bc4c2d86a7b3c24ee5bc452964d2c5dc98
AnSunWoo <110546006+Sunja-An@users.noreply.github.com>
2022-10-16 14:53:18 +0000

    Modify the code for cleaning design and showing the data cleanly

--- WEB(FE)/ar/src/components/Home/UserLocCard.js
+++ WEB(FE)/ar/src/components/Home/UserLocCard.js
@@ -1,29 +1,53 @@
-import * as React from 'react';
+import React from 'react';
+import styled from 'styled-components';
+
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
+const LocCardStyle = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  margin-top: 30px;
+  width: calc(100%);
+  margin-left: 30px;
+  margin-right: 30px;
+  margin-bottom: 30px;
+
+  .user-info{
+    box-sizing: border-box;
+    flex-direction-row;
+    padding-left:10px;
+    padding-right:10px;
+    padding-top: 10px;
+  }
+`
 
 const UserLocCard = (User) => {
   return (
-    <Card sx={{ minWidth: 300 }}>
-      <CardContent>
-        <Typography variant="h5" component="div">
-
-        </Typography>
-        <Typography variant="body2">
-          <span>계급 : {User.Class}</span> 
-          <span>이름 : {User.Name}</span> 
-          <br />
-          <span>장소 : {User.Located}</span>
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+    <LocCardStyle>
+      <div className="card-design">
+        <Card>
+          <CardContent className='user-info'>
+            <Typography variant="body2">
+              <span>계급 : {User.Class}</span> 
+              <span>이름 : {User.Name}</span> 
+              <br />
+              <span>장소 : {User.Located}</span>
+            </Typography>
+          </CardContent>
+          <CardActions className="command-btn">
+            <Button size="small">복귀</Button>
+            <Button size="small">집합</Button>
+          </CardActions>
+        </Card>
+      </div>
+    </LocCardStyle>
+    
   );
 };
 
