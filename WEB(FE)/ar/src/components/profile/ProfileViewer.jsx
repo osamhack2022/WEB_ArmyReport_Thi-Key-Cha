a494a03bb205795b7983b88273f90ef4abf3da99
GyeongChan <40288646+gclogs@users.noreply.github.com>
2022-10-29 14:32:45 +0000

    refactor: `ProfileTabs` props forwarding

--- WEB(FE)/ar/src/components/profile/ProfileViewer.jsx
+++ WEB(FE)/ar/src/components/profile/ProfileViewer.jsx
@@ -1,47 +1,22 @@
 import React from 'react';
-import db from '../../database/DB_Manager'
-import { doc, collection, getDocs, updateDoc, deleteDoc } from 'firebase/firestore';
 import useHeader from '../base/hooks/useHeader';
 import ProfileUserContainer from './ProfileUserContainer'
-import ProfileTabs from './ProfileTabs'
+import ProfileTabs from './ProfileTab'
 import Header from '../base/Header'
 import Footer from '../base/Footer'
 
-
-export class Firestore {
-  constructor(collection) {
-    this.collection = collection;
-  }
-
-  async fetchCollection() {
-    const collRef = collection(db, this.collection);
-    const Snap = await getDocs(collRef);
-    const data = Snap.docs.map(doc => ({
-      ...doc.data(),
-      id: doc.id
-    }))
-    return data;
-  }
-
-  async updateCollection(user, updateContent) {
-    const collRef = await updateDoc(doc(db, this.collection, user.uid), {
-      content: updateContent
-  });
-  }
-
-  async deleteCollection(user) {
-    await deleteDoc(doc(db, this.collection, user.uid));
-  }
-}
-
 const ProfileViewer = () => {
   const { user } = useHeader();
+  const listType = {
+    letter: 'post-letters',
+    suggest: 'post-suggests'
+  }
 
   return (
     <>
       <Header />
       <ProfileUserContainer user={user.data}/>
-      <ProfileTabs />
+      <ProfileTabs type={listType} uid={user.uid}/>
       <Footer />
     </>
   )
